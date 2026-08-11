0f094911fd2560c99768b1e0d9f65a36e1417902
AMY LEE REBEKA DE SOUZA SANTOS <53581685833@info.local>
2026-08-11 10:01:47 -0300

    React List

--- React Native/ReactlistNative/src/componets/taskItem/TaskItem.jsx
+++ React Native/ReactlistNative/src/componets/taskItem/TaskItem.jsx
@@ -3,7 +3,7 @@ import { TaskItemStyles } from "./TaskItemStyles"
 
 export const TaskItem = () => {
     return (
-        <View style={TaskItemStyles.taskItem}>
+        <View style={TaskItemStyles.cardBox}>
             <Text>Task Item Component</Text>
         </View>
     )
